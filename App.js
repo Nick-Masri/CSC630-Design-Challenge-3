f058f9d72369739de0e7473e7fc93327c8b8c689
unknown <masrin123@hotmail.com>
2019-04-15 12:46:33 -0400

    started creating styles and game class

--- App.js
+++ App.js
@@ -1,21 +1,117 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Image, View, StyleSheet, Text } from 'react-native';
 
-export default class App extends React.Component {
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 1,
+  },
+  appName: {
+    fontSize: 60,
+    color: '#153944',
+    fontFamily: 'Roboto',
+    marginTop: 50,
+  },
+  image: {
+    width: 350,
+    height: 250,
+    resizeMode: "stretch",
+    alignItems: 'center',
+    margin: 'auto',
+    marginTop: 20,
+  },
+  dashesContainer: {
+    justifyContent: 'center',
+    width: '80%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  word: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  dashes: {
+    borderBottomWidth: 3,
+    width: 50,
+    margin: 5,
+    height: 50,
+    flexDirection: 'column',
+    justifyContent: 'flex-end'
+  },
+  hintBox: {
+    marginTop: 30,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: '#d3d3d3',
+  },
+  hint: {
+    fontSize: 24,
+  },
+  letter: {
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 5,
+  }
+});
+
+export default class Bananas extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      maxGuesses: 8,
+      numberWrong: 0,
+      numberRight: 0,
+      usedLetters: [],
+      remainingLetter: [],
+      answer: "",
+      hint: "",
+    }
+
+    initializeGame = () => {
+      this.setState({gameState:
+        [
+          
+        ]
+
+      });
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.appName}>HANGMAN</Text>
+        <Image source={require('./assets/hangman.jpg')} style={styles.image}/>
+
+        <View style={styles.dashesContainer}>
+          <View style={styles.word}>
+            <View style={styles.dashes} />
+            <View style={styles.dashes} />
+            <View style={styles.dashes} />
+            <View style={styles.dashes} />
+          </View>
+          <View style={styles.word}>
+            <View style={styles.dashes}><Text style={styles.letter}>a</Text></View>
+            <View style={styles.dashes} />
+            <View style={styles.dashes} />
+          </View>
+        </View>
+
+        <View style={styles.hintBox}>
+          <Text style={styles.hint}>
+          Hint: brown eyes
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
